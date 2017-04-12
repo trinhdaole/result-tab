760a95ad9aa14572b2564c0849b38fabb7f70806
long <longng@nexlesoft.com>
2017-04-12 14:40:45 +0700

    cm page

--- src/components/map-finder/map-finder-page-component.js
+++ src/components/map-finder/map-finder-page-component.js
@@ -1,3 +1,48 @@
 /**
  * Created by long on 4/12/17.
  */
+"use strict";
+
+import React, {Component, PropTypes} from "react";
+import Popup from '../wrapper/map-finder/map-finder-popup-component'
+
+
+export default class MapFinderPageComponent extends Component {
+    constructor(props) {
+
+
+        super(props);
+        this.state = {
+
+        };
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount(){
+
+    }
+
+
+
+    render() {
+
+
+        return (
+            <Popup  />
+
+        );
+
+    }
+
+
+}
+
+MapFinderPageComponent.propTypes = {
+
+
+
+};
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,9 +1,10 @@
 import React, { PropTypes } from 'react'
 import Header from '../components/header'
+import MapFinder from '../components/map-finder/map-finder-page-component'
 
 const App = () => (
     <div>
-        <Header  />
+        <MapFinder  />
     </div>
 )
 
