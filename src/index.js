6c4533da20d0c27fb07b22f1d37f14851d70d0c7
NhatMinh Nguyen <minhnn@nexlesoft.com>
2017-04-20 17:17:27 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './containers/app'
-import './components/stylesheets/App.css'
-//const store = createStore(reducer)
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './containers/app';
+import './components/stylesheets/App.css';
+import reducer from './reducers'
 
+const store = createStore(reducer)
 render(
-    <Provider >
+    <Provider store={store} >
         <App />
     </Provider>,
     document.getElementById('root')
